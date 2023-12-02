a6f7382bbbf388692ff29dd60d947cab7a4811c6
bannzai <kingkong999yhirose@gmail.com>
2023-12-02 20:19:22 +0900

    add attach

--- Sources/DataWrapper/DataWrapper.swift
+++ Sources/DataWrapper/DataWrapper.swift
@@ -1,2 +1,5 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+@attached(member, names: arbitrary)
+public macro DataModel() = #externalMacro(module: "DataWrapperPlugin", type: "DataModelMacro")
